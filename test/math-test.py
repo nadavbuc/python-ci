eafe2f067e52845d28b37bdbed7454461e279d5c
nadav <nadav@expim.co.il>
2018-08-20 09:21:53 +0300

    success

--- test/math-test.py
+++ test/math-test.py
@@ -4,4 +4,4 @@ from src.math import Math
 
 class Mathtest(unittest.TestCase):
     def test_addition(self):
-        self.assertEqual(Math.addition(3,4), 8)
+        self.assertEqual(Math.addition(3,4), 7)
